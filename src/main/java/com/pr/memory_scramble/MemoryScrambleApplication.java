88724f628b9b9a193d12f93c0fc9eb6de29030ef
mcittkmims <adrian.vremere.swe@gmail.com>
2025-11-05 19:47:46 +0200

    init

---
+++ src/main/java/com/pr/memory_scramble/MemoryScrambleApplication.java
@@ -0,0 +1,13 @@
+package com.pr.memory_scramble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryScrambleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryScrambleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pr/memory_scramble/MemoryScrambleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pr.memory_scramble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryScrambleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
